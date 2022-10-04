d6e9b57af507afb5ec9e2421a77bf4cb42ceb3e6
LAEL <lael@gmail.com>
2022-10-04 08:33:46 -0500

    AGREGUE LA PRACTICA 10

---
+++ src/practica2/DIEZ.java
@@ -0,0 +1,43 @@
+package practica2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class DIEZ {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DIEZ window = new DIEZ();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DIEZ() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
